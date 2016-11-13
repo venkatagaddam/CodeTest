7cdf336cfaed48e65d937117a6ba417c5898b3c2
venkatagaddam <venkata.uk@gmail.com>
2016-11-13 20:47:04 +0000

    Changes to Jasmine Tests - Changes to string enxtensions js file.

--- CodingTest/JasmineTests/scripts/string-extensions.js
+++ CodingTest/JasmineTests/scripts/string-extensions.js
@@ -1,4 +1,26 @@
-﻿String.prototype.capitalise = function () {}
-String.prototype.camelCaseToSpineCase = function () { }
-String.prototype.spineCaseToCamelCase = function () { }
-String.prototype.format = function () { }
+﻿String.prototype.capitalise = function () {
+    return this.replace(/^[a-z]/, l => l.toUpperCase());
+}
+
+String.prototype.camelCaseToSpineCase = function () {
+    var noCamel = this.replace(/(\B[A-Z])/g, ' $1');
+    var newSpine = noCamel.replace(/\s|_/g, "-");
+    return newSpine.toLowerCase();
+}
+
+String.prototype.spineCaseToCamelCase = function () {
+    var str = this.replace(/([-][a-z])/g, function ($1) { return $1.toUpperCase().replace('/[-]/', ''); });
+    str = str.replace(/(^|\s)[a-z]/g, function (f) { return f.toUpperCase(); })
+    str = str.replace(/-/g, '').replace(/ /g, function ($1) { return $1.toUpperCase().replace(/ /g, ''); });
+    return str.replace(/^(.)/, function ($1) { return $1.toLowerCase(); })
+}
+
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+          ? args[number]
+          : match
+        ;
+    });
+}
